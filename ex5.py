15172390071f8f34499e461ec894f17dae39cdac
Devyanshu Jadon <divyanshu.xa@gmail.com>
2023-11-12 22:12:41 +0530

    ex5 complete
    Signed-off-by: Devyanshu Jadon <divyanshu.xa@gmail.com>

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.") # Let's talk about Zed A. Shaw.
+print(f"He's {my_height} inches tall.") # He's 74 inches tall.
+print(f"He's {my_weight} pounds heavy.") # He's 180 pounds heavy.
+print("Actually that's not too heavy.") # Actually that's not too heavy.
+print(f"He's got {my_eyes} eyes and {my_hair} hair.") # He's got Blue eyes and Brown hair.
+print(f"His teeth are usually {my_teeth} depending on the coffee.") # His teeth are usually White depending on the coffee.
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight # 289
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.") # If I add 35, 74, and 180 I get 289.
